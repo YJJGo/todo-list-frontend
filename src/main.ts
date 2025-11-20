cce2c5f98425f39ea5dd96fa5b86bc6e81c080cc
yangjj <1489027718@qq.com>
2025-11-20 17:13:49 +0800

    【feature】引入element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+
+// 引入 Element Plus 样式
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+// 引入所有图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+// 注册所有图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
+app.use(ElementPlus)
 app.mount('#app')
